110476340276a5f1ed990c8422b859d20318b537
FrancoisTelecom <francois.5@hotmail.fr>
2017-02-12 21:14:53 +0100

    class ville

--- eclipseOC/src/firstLesson/Init.java
+++ eclipseOC/src/firstLesson/Init.java
@@ -1,9 +1,21 @@
 package firstLesson;
 
+import java.util.Scanner;
+import java.util.*;
+
 public class Init {
+	public static String titi;
 
 	public static void main(String[] args) {
 		System.out.println("toto");
+	}
+	
+	public String returnTiti(){
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Veillez saisir un mot de passe");
+		String str = sc.nextLine();
+		return "Vous avez saisi : "+ str;
+		
 	}
 
 }
