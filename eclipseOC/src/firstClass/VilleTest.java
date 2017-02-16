e706dd644c8182243d49ca268031413ebbdafcd2
FrancoisTelecom <francois.5@hotmail.fr>
2017-02-16 18:08:54 +0100

    test travis

--- eclipseOC/src/firstClass/VilleTest.java
+++ eclipseOC/src/firstClass/VilleTest.java
@@ -1,17 +1,18 @@
 package firstClass;
 
 import static org.junit.Assert.*;
-
+ 
 import org.junit.Test;
 
 import junit.framework.Assert;
-
-public class VilleTest extends Main {
+ 
+public class VilleTest {
+    static int nbHab = 1234567 ;
 
 	@Test
 	public void test() {
 		Ville ville1 = new Ville("Paris", 1234567, "France");
-		assertEquals(1234567 , ville1.getNbHabitants());
+		assertEquals(nbHab, ville1.getNbHabitants());
 		//fail("Not yet implemented");
 	}
 
